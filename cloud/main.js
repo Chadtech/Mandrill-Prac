30ae4cecebfda55beb81a9be46caea63215388a7
Chadtech <chadstearns1@gmail.com>
2015-01-21 11:54:15 -0700

    Cleaned up main.coffee to a more natural code flow

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,4 @@ client = Mandrill new Mandrill.Mandrill'd7rEYQJY5JVJe7VraonWyA')
 Parse.Cloud.define("WHATUP", function(request, response) {
   response.success("WHATUPWHATUPWHATUPWHATUPWHATUP!");
 });
+gig
